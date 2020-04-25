3d8fcd7feec868e3d619492ba65e2211de708605
Stephen Boyle <snboyle@berkeley.edu>
2020-04-25 02:52:49 -0700

    Initial Commit

---
+++ Gimme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gimme
+//
+//  Created by Stephen Boyle on 4/25/20.
+//  Copyright © 2020 Stephen Boyle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
