1b460a45a6ac5ffd600a8784f0fc492421aa0633
Stephen Boyle <snboyle@berkeley.edu>
2020-04-25 10:27:50 -0700

    Got rid of SwiftUI because not enough time to learn

---
+++ Gimme/Model.h
@@ -0,0 +1,13 @@
+//
+//  Model.h
+//  Gimme
+//
+//  Created by Stephen Boyle on 4/25/20.
+//  Copyright © 2020 Stephen Boyle. All rights reserved.
+//
+
+#ifndef Model_h
+#define Model_h
+
+
+#endif /* Model_h */
---
+++ Gimme/ViewController/LandingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingViewController.swift
+//  Gimme
+//
+//  Created by Stephen Boyle on 4/25/20.
+//  Copyright © 2020 Stephen Boyle. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("here")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
